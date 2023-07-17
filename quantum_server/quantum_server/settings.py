e30ed358dd0d38c237603c9bac42389845e4325e
Justin <jandersen770@gmail.com>
2023-07-17 16:19:06 -0700

    Update origin

--- quantum_server/quantum_server/settings.py
+++ quantum_server/quantum_server/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://jbauce.com/ai_tarot/"
+    "http://jbauce.com"
 ]
 
 CORS_ALLOW_METHODS = [
